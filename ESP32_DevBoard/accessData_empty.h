588707708e5081b68edbc0b1729fe606c620ee87
Schiller <arno-schiller@hotmail.de>
2021-02-23 18:39:33 +0100

    Included empty accessData file

---
+++ ESP32_DevBoard/accessData_empty.h
@@ -0,0 +1,10 @@
+// WiFi configuration 
+const char* ssid     = "********";      // name of the WiFi network
+const char* password = "********";      // password of the WiFi network
+
+// MQTT configuration 
+const char* mqtt_server = "127.0.0.1";  // ip adress or domain like:
+//const char* mqtt_server = "mqtt-broker.domain.de";
+#define mqtt_port 1833                  // often the port is 1883
+#define MQTT_USER "username"
+#define MQTT_PASSWORD "password123"
